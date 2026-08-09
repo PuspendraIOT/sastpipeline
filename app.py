9c98b11aeb33c07751f110b6c7b86b08cc67484d
Puspendra Yadav <65920009+PuspendraIOT@users.noreply.github.com>
2026-08-09 21:09:25 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+print("Starting our dummy application...")
+
+# BUG: This hardcoded password is a security risk!
+DATABASE_PASSWORD = "my_super_secret_password_123"
+
+def login():
+    print("Connecting to the database...")
+
+login()
